df265a7519397656ddc9fcc8265bbd69ad0ef89f
Wayne Hwang <wonyong@wonyong.net>
2022-01-18 16:38:06 +0900

    Update Exchanges.jsx

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,54 +1,33 @@
 import React from 'react';
 import millify from 'millify';
-import { Collapse, Row, Col, Typography, Avatar } from 'antd';
-import HTMLReactParser from 'html-react-parser';
-
-import { useGetExchangesQuery } from '../services/cryptoApi';
+import { Row, Col, Statistic } from 'antd';
+import { useGetStatsQuery } from '../services/cryptoApi';
 import Loader from './Loader';
 
-const { Text } = Typography;
-const { Panel } = Collapse;
-
+// const { Title } = Typography;
 const Exchanges = () => {
-  const { data, isFetching } = useGetExchangesQuery();
-  const exchangesList = data?.data?.exchanges;
+  const { data, isFetching } = useGetStatsQuery();
+  const exchangesList = data?.data;
  // Note: To access this endpoint you need premium plan
   if (isFetching) return <Loader />;
-
   return (
     <>
-      <Row>
-        <Col span={6}>Exchanges</Col>
-        <Col span={6}>24h Trade Volume</Col>
-        <Col span={6}>Markets</Col>
-        <Col span={6}>Change</Col>
-      </Row>
-      <Row>
-        {/* {exchangesList.map((exchange) => (
-          <Col span={24}>
-            <Collapse>
-              <Panel
-                key={exchange.uuid}
-                showArrow={false}
-                header={(
-                  <Row key={exchange.uuid}>
-                    <Col span={6}>
-                      <Text><strong>{exchange.rank}.</strong></Text>
-                      <Avatar className="exchange-image" src={exchange.iconUrl} />
-                      <Text><strong>{exchange.name}</strong></Text>
-                    </Col>
-                    <Col span={6}>${millify(exchange.volume)}</Col>
-                    <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
-                    <Col span={6}>{millify(exchange.marketShare)}%</Col>
-                  </Row>
-                  )}
-              >
-                {HTMLReactParser(exchange.description || '')}
-              </Panel>
-            </Collapse>
-          </Col>
-        ))} */}
-      </Row>
+      <div className="exchange-container">
+        <Row gutter={[32, 32]}>
+          <Col span={12}><Statistic title="Total Coins" value={exchangesList.totalCoins} /></Col>
+          <Col span={12}><Statistic title="Total Markets" value={exchangesList.totalMarkets} /></Col>
+          <Col span={12}><Statistic title="Total Exchanges" value={exchangesList.totalExchanges} /></Col>
+          <Col span={12}><Statistic title="Total Market Caps" value={millify(exchangesList.totalMarketCap)} /></Col>
+          <Col span={12}><Statistic title="Total 24h Volumes" value={millify(exchangesList.totalMarketCap)} /></Col>
+        </Row>
+      </div>
+      {/* <div className="coin-heading-container">
+        <p>Total Coins : {exchangesList.totalCoins}</p>
+        <p>Total Markets : {exchangesList.totalMarkets}</p>
+        <p>Total Exchanges : {exchangesList.totalExchanges}</p>
+        <p>Total Market Caps : {millify(exchangesList.totalMarketCap)}</p>
+        <p>Total 24h Volumes : {millify(exchangesList.totalMarketCap)}</p>
+      </div> */}
     </>
   );
 };
